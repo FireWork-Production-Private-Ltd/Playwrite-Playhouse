0b011c3a522297a6e1b849e2e6ae7881fb66fc9e
Utsav Patel <utsav.patel1512@outlook.com>
2024-02-08 00:07:20 +0530

    api testing

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,16 +1,36 @@
-import { test as setup, expect } from '@playwright/test';
+// import { test as setup, expect } from '@playwright/test';
+
+// const authFile = 'playwright/.auth/user.json';
+
+// setup('authenticate', async ({ page }) => {
+//     await page.goto('https://qa-utsav-patel.rt.gw/wp-login.php?redirect_to=https%3A%2F%2Fqa-utsav-patel.rt.gw%2Fwp-admin%2F&reauth=1');
+//     await page.getByLabel('Username or Email Address').fill('qa-utsav-patel');
+//     await page.getByLabel('Username or Email Address').press('Tab');
+//     await page.getByLabel('Password', { exact: true }).fill('.2123cds*sKird');
+//     await page.getByRole('button', { name: 'Log In' }).click();
+//     // await page.waitForURL('https://qa-utsav-patel.rt.gw/');
+
+//   // End of authentication steps.
+
+//   await page.context().storageState({ path: authFile });
+// });
+
+
+import { test as setup } from '@playwright/test';
+import { title } from 'process';
 
 const authFile = 'playwright/.auth/user.json';
 
-setup('authenticate', async ({ page }) => {
-    await page.goto('https://qa-utsav-patel.rt.gw/wp-login.php?redirect_to=https%3A%2F%2Fqa-utsav-patel.rt.gw%2Fwp-admin%2F&reauth=1');
-    await page.getByLabel('Username or Email Address').fill('qa-utsav-patel');
-    await page.getByLabel('Username or Email Address').press('Tab');
-    await page.getByLabel('Password', { exact: true }).fill('.2123cds*sKird');
-    await page.getByRole('button', { name: 'Log In' }).click();
-    // await page.waitForURL('https://qa-utsav-patel.rt.gw/');
-
-  // End of authentication steps.
-
-  await page.context().storageState({ path: authFile });
+setup('authenticate', async ({ request }) => {
+  // Send authentication request. Replace with your own.
+  await request.post('https://qa-utsav-patel.rt.gw/wp-json/wp/v2/posts', {
+    headers: {
+      'Authorization' : 'Basic cWEtdXRzYXYtcGF0ZWw6YXdCSiBQd2lVIGNqWGkgWmlHaSBkdXpEIElDRVg='
+    },
+    data: {
+      title: 'Test Post playwrite utsav patel',
+      status: 'publish',
+    },
+  });
+  await request.storageState({ path: authFile });
 });
